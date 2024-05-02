57905b22ccdfc579ea841f991dd7f2da544a6b89
keshavsaini07 <sainikeshav770@gmail.com>
2024-05-02 19:16:05 +0530

    Added get flights api

--- src/controllers/flight-controller.js
+++ src/controllers/flight-controller.js
@@ -37,6 +37,10 @@ async function createFlight(req, res) {
 }
 }
 
+/*
+* GET : /flights/
+* req-body {}
+*/ 
 async function getAllFlights(req, res){
     try {
         const flights = await FlightService.getAllFlights(req.query);
@@ -48,7 +52,24 @@ async function getAllFlights(req, res){
     }
 }
 
+/*
+* GET : /flights/:id
+* req-body {}
+*/ 
+async function getFlight(req, res) {
+  try {
+    const flight = await FlightService.getFlight(req.params.id);
+    SuccessResponse.data = flight;
+    return res.status(StatusCodes.OK).json(SuccessResponse);
+  } catch (error) {
+    console.log(error)
+    ErrorResponse.error = error;
+    return res.status(error.statusCode).json(ErrorResponse);
+  }
+}
+
 module.exports = {
   createFlight,
-  getAllFlights
+  getAllFlights,
+  getFlight,
 };
--- src/routes/v1/flight-routes.js
+++ src/routes/v1/flight-routes.js
@@ -17,4 +17,10 @@ router.get(
   FlightController.getAllFlights
 );
 
+// /api/v1/flights/:id - GET
+router.get(
+  "/:id",
+  FlightController.getFlight
+);
+
 module.exports = router;
